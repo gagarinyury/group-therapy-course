65c2ffd85a9259dd48bc9adb081559636ced996d
Example User <example@example.com>
2025-05-19 10:10:04 +0000

    Fix Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CourseLanding } from './components/course/CourseLanding';
 import { LessonContainer } from './components/lesson/LessonContainer';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<CourseLanding />} />
-        <Route path="/lesson/:lessonId" element={<LessonContainer />} />
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router basename="/group-therapy-course">
+        <Routes>
+          <Route path="/" element={<CourseLanding />} />
+          <Route path="/lesson/:lessonId" element={<LessonContainer />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
